6629aa9e230c706d40fe3e2601bcca48f183aecc
scino11 <130865684+scino11@users.noreply.github.com>
2023-06-27 18:41:19 -0300

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
 
-// se importa la clase scanner
+// se importa la clase scanner y la clase ArrayList
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    // se declaran las variables
+    // se declaran las variables, un scanner, y una variable ll lamada historia que
+    // es un arraylist.
     static Scanner scanner = new Scanner(System.in);
     static int contador = 0;
     static String nombre, logeo;
@@ -11,8 +13,9 @@ public class App {
     static int montoinicial = 1000;
     static int operacion, cbu;
     static double ingreso, retiro;
+    static ArrayList<String> historial = new ArrayList<>();
 
-    // se declara el metodo para crear el usuario
+    // se declara el metodo para crear el usuario.
     public static void crearUsuario() {
         System.out.println("¡Bienvenido al cajero automático!");
         System.out.println("Para crear una nueva cuenta por favor siga los siguientes pasos.");
@@ -24,19 +27,20 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-        // se llama al metodo crearUsuario
+        // se llama al metodo crearUsuario.
         crearUsuario();
 
         System.out.print("Tiene tres intentos para ingresar a su cuenta.");
 
         do {
+            System.out.println();
             System.out.print("Por favor, ingrese su nombre completo: ");
             nombre = scanner.nextLine();
             contador++;
             System.out.print("Por favor, ingrese la contraseña de su cuenta: ");
             password = scanner.nextLine();
             // se utiliza .equals para comprarar los datos ingresados con el nombre y la
-            // password
+            // password.
             if (nombre.equals(logeo) && password.equals(logeo2)) {
                 System.out.println();
                 System.out.println("Accedió correctamente a su cuenta.");
@@ -52,13 +56,13 @@ public class App {
 
         if (contador >= 3) {
             System.out.println("Excedió el límite de intentos.");
-            // se corta el codigo cuando se ingresa tres veces mal
+            // se corta el codigo cuando se ingresa tres veces mal.
             return;
         }
         System.out.println("Usted cuenta con 1000$ en la cuenta.");
         System.out.println();
 
-        // se inicia el bucle do-while del cajero
+        // se inicia el bucle do-while del cajero.
         do {
             System.out.println();
             System.out.println("Por favor, seleccione la opción que desea realizar: ");
@@ -76,6 +80,8 @@ public class App {
                     montoinicial += ingreso;
                     System.out.println(
                             "Usted ingreso: " + ingreso + "$" + " y le corresponde un saldo de: " + montoinicial + "$");
+                    // historial.add agrega el ingreso al arrayList.
+                    historial.add("Ingresó " + ingreso + "$");
                     break;
 
                 case 2:
@@ -87,6 +93,8 @@ public class App {
                     } else {
                         montoinicial -= retiro;
                         System.out.println("Usted retiró: " + retiro + " y tiene un saldo de: " + montoinicial);
+                        // historial.add agrega el retiro al arrayList.
+                        historial.add("Retiró " + retiro + "$");
                     }
                     break;
                 case 3:
@@ -97,8 +105,15 @@ public class App {
                 default:
                     System.out.println("La opción ingresada no es válida. Por favor, ingrese nuevamente.");
             }
-            // se repite el bucle hasta que el usuario eliga la opcion numero 3
+            // se repite el bucle hasta que el usuario eliga la opcion numero 3.
         } while (operacion != 3);
+
+        // al final se muestra el historial de transacciones con un for mejorado.
+
+        System.out.println("Historial de transacciones:");
+        for (String transaccion : historial) {
+            System.out.println(transaccion);
+        }
 
         scanner.close();
     }
